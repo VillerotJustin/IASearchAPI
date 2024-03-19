b2b23f1a55ea40255a773a9d22b199c66b9a436a
Villerot Justin <justinvillerot@orange.fr>
2024-03-19 13:46:01 +0100

    Update environment.py

--- app/utils/environment.py
+++ app/utils/environment.py
@@ -30,5 +30,7 @@ class Config:
     ALGORITHM = os.environ.get('ALGORITHM', "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES', 10_080))  # one week
 
+    LOAD_LOCAL_MODEL = os.environ.get('LOAD_LOCAL_MODEL', False)
+
 
 settings = Config()
